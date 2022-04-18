67405f88cb755b049d1c3951468b58b1b5a702bc
Andrey <andreystar2403@gmail.com>
2022-04-18 12:43:50 +0300

    Store results in table.csv for each run.

--- src/main/java/org/softwareheritage/graph/tinkerpop/Benchmark.java
+++ src/main/java/org/softwareheritage/graph/tinkerpop/Benchmark.java
@@ -7,16 +7,26 @@ import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 import org.apache.tinkerpop.gremlin.process.traversal.util.Metrics;
 import org.apache.tinkerpop.gremlin.process.traversal.util.TraversalMetrics;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
 import org.softwareheritage.graph.SwhBidirectionalGraph;
 import org.webgraph.tinkerpop.WebgraphGremlinQueryExecutor;
 import org.webgraph.tinkerpop.structure.WebGraphGraph;
 import org.webgraph.tinkerpop.structure.provider.SimpleWebGraphPropertyProvider;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class Benchmark {
 
@@ -27,6 +37,11 @@ public class Benchmark {
     private final WebgraphGremlinQueryExecutor e;
     private final long samples;
     private final int iters;
+
+    private final Map<String, Supplier<BenchmarkQuery>> queries = Map.of(
+            "earliestContainingCommit", EarliestContainingCommit::new,
+            "recursiveContentPathsWithPermissions", RecursiveContentPathsWithPermissions::new,
+            "snapshotRevisionsWithBranches", SnapshotRevisionsWithBranches::new);
 
     public static void main(String[] args) throws IOException, JSAPException {
         SimpleJSAP jsap = new SimpleJSAP(Benchmark.class.getName(),
@@ -71,21 +86,34 @@ public class Benchmark {
         this.e = new WebgraphGremlinQueryExecutor(graph);
     }
 
-    private <S, E> void profileVertexQuery(List<Long> startIds, Function<Long, Function<GraphTraversalSource, GraphTraversal<S, E>>> query) {
-        profileVertexQuery(startIds, query, false);
-    }
-
-    private <S, E> void profileVertexQuery(List<Long> startIds, Function<Long, Function<GraphTraversalSource, GraphTraversal<S, E>>> query, boolean printMetrics) {
+    /*
+    2021-snapshot
+        311702.txt
+     */
+    private <S, E> void profileVertexQuery(List<Long> startIds, BenchmarkQuery query, boolean printMetrics) throws IOException {
         System.out.println("Profiling query for ids: " + startIds + "\n");
+        Path dir = Path.of("benchmarks")
+                       .resolve(Instant.now().truncatedTo(ChronoUnit.SECONDS).toString() + "-" + query.getName());
+        Files.createDirectories(dir);
+        System.out.println("Results will be saved at: " + dir);
         long totalMs = 0;
         long max = 0;
         long maxId = 0;
+        StringBuilder csvLine = new StringBuilder("id,elements");
+        for (int i = 0; i < iters; i++) {
+            csvLine.append(",").append("run").append(i + 1);
+        }
+        try (BufferedWriter bw = Files.newBufferedWriter(dir.resolve("table.csv"), StandardCharsets.UTF_8,
+                StandardOpenOption.CREATE)) {
+            bw.write(csvLine.append("\n").toString());
+        }
         for (long id : startIds) {
             System.out.println("Running query for id: " + id);
-            LongLongImmutablePair stat = statsForQuery(query, id, iters, printMetrics);
+
+            LongLongImmutablePair stat = statsForQuery(query.getQuery(), id, iters, printMetrics, dir);
             long average = stat.leftLong();
             long elements = stat.rightLong();
-            double perElement = 1.0 * average / elements;
+            double perElement = elements !=0 ? 1.0 * average / elements : 0;
             totalMs += average;
             if (max < average) {
                 max = average;
@@ -98,19 +126,35 @@ public class Benchmark {
         System.out.printf("Max time: %dms for id %d%n", max, maxId);
     }
 
-    private <S, E> LongLongImmutablePair statsForQuery(Function<Long, Function<GraphTraversalSource, GraphTraversal<S, E>>> query, long id, long iters, boolean printMetrics) {
+    private <S, E> LongLongImmutablePair statsForQuery(Function<Long, Function<GraphTraversalSource, GraphTraversal<S, E>>> query, Long id, long iters, boolean printMetrics, Path dir) throws IOException {
         long totalMsPerId = 0;
-        long elements = 0;
+        long elements = -1;
+
+        StringBuilder csvLine = new StringBuilder(id.toString());
+        Path idDir = dir.resolve(Long.toString(id));
+        Files.createDirectories(idDir);
         for (int i = 0; i < iters; i++) {
             System.out.println(i + 1 + "/" + iters);
             TraversalMetrics metrics = profile(query.apply(id));
-            if (printMetrics){
+            if (printMetrics) {
                 System.out.println(metrics);
             }
-            elements = getLastMetric(metrics).getCount("elementCount");
+            try (BufferedWriter bw = Files.newBufferedWriter(idDir.resolve(i + 1 + ".txt"), StandardCharsets.UTF_8)) {
+                bw.write(metrics.toString());
+            }
+            Long elementCount = getLastMetric(metrics).getCount("elementCount");
+            if (elements == -1) {
+                elements = elementCount != null ? elementCount : 0;
+                csvLine.append(",").append(elements);
+            }
             long durationMs = metrics.getDuration(TimeUnit.MILLISECONDS);
+            csvLine.append(",").append(durationMs);
             System.out.println("Finished in: " + durationMs + "ms. Results: " + elements);
             totalMsPerId += durationMs;
+        }
+        try (BufferedWriter bw = Files.newBufferedWriter(dir.resolve("table.csv"), StandardCharsets.UTF_8,
+                StandardOpenOption.APPEND)) {
+            bw.write(csvLine.append("\n").toString());
         }
         return new LongLongImmutablePair(totalMsPerId / iters, elements);
     }
@@ -120,18 +164,12 @@ public class Benchmark {
         return metrics1.get(metrics1.size() - 1);
     }
 
-    private void runQueryByName(String name, long arg) {
-        String label;
-        if (name.equals("earliestContainingCommit")) {
-            label = "CNT";
-        } else if (name.equals("recursiveContentPathsWithPermissions")) {
-            label = "REV";
-        } else if (name.equals("snapshotRevisionsWithBranches")) {
-            label = "SNP";
-        } else {
+    private void runQueryByName(String name, long arg) throws IOException {
+        if (!queries.containsKey(name)) {
             System.out.println("Unknown query name: " + name);
             return;
         }
+        BenchmarkQuery query = queries.get(name).get();
 
         boolean printMetrics = false;
         List<Long> startIds;
@@ -141,15 +179,9 @@ public class Benchmark {
             printMetrics = true;
         } else {
             System.out.println("Generating starting points...");
-            startIds = randomVerticesWithLabel(label, samples);
+            startIds = query.generateStartingPoints();
         }
-        if (name.equals("earliestContainingCommit")) {
-            profileVertexQuery(startIds, Query::earliestContainingCommit, printMetrics);
-        } else if (name.equals("recursiveContentPathsWithPermissions")) {
-            profileVertexQuery(startIds, Query::recursiveContentPathsWithPermissions, printMetrics);
-        } else if (name.equals("snapshotRevisionsWithBranches")) {
-            profileVertexQuery(startIds, Query::snapshotRevisionsWithBranches, printMetrics);
-        }
+        profileVertexQuery(startIds, query, printMetrics);
     }
 
     private <S, E> TraversalMetrics profile(Function<GraphTraversalSource, GraphTraversal<S, E>> query) {
@@ -161,6 +193,66 @@ public class Benchmark {
                            .order().by(Order.shuffle)
                            .limit(count)
                            .id().map(id -> (long) id.get()));
+    }
+
+
+    private class EarliestContainingCommit implements BenchmarkQuery<Long, Vertex, Vertex> {
+        @Override
+        public String getName() {
+            return "earliestContainingCommit";
+        }
+
+        @Override
+        public Function<Long, Function<GraphTraversalSource, GraphTraversal<Vertex, Vertex>>> getQuery() {
+            return Query::earliestContainingCommit;
+        }
+
+        @Override
+        public List<Long> generateStartingPoints() {
+            return randomVerticesWithLabel("SNP", samples);
+        }
+    }
+
+    private class RecursiveContentPathsWithPermissions implements BenchmarkQuery<Long, Vertex, String> {
+        @Override
+        public String getName() {
+            return "recursiveContentPathsWithPermissions";
+        }
+
+        @Override
+        public Function<Long, Function<GraphTraversalSource, GraphTraversal<Vertex, String>>> getQuery() {
+            return Query::recursiveContentPathsWithPermissions;
+        }
+
+        @Override
+        public List<Long> generateStartingPoints() {
+            return randomVerticesWithLabel("REV", samples);
+        }
+    }
+
+    private class SnapshotRevisionsWithBranches implements BenchmarkQuery<Long, Vertex, String> {
+        @Override
+        public String getName() {
+            return "snapshotRevisionsWithBranches";
+        }
+
+        @Override
+        public Function<Long, Function<GraphTraversalSource, GraphTraversal<Vertex, String>>> getQuery() {
+            return Query::snapshotRevisionsWithBranches;
+        }
+
+        @Override
+        public List<Long> generateStartingPoints() {
+            return randomVerticesWithLabel("SNP", samples);
+        }
+    }
+
+    interface BenchmarkQuery<T, S, E> {
+        String getName();
+
+        Function<T, Function<GraphTraversalSource, GraphTraversal<S, E>>> getQuery();
+
+        List<T> generateStartingPoints();
     }
 
 }
